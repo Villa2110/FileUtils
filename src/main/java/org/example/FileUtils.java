87b376e4f121b89a3d2c8b4d9ddb3acfd02dc1ce
jose <jose_francisco_villa@hotmail.com>
2022-09-25 12:43:03 -0500

    Agregando pruebas unitarias

--- src/main/java/org/example/FileUtils.java
+++ src/main/java/org/example/FileUtils.java
@@ -2,26 +2,36 @@ package org.example;
 
 import javax.swing.*;
 import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
 
 public class FileUtils {
 
     public static void leerArchivo(String path) throws FileNotFoundException {
-        FileReader archivo=new FileReader(path);
-        try {
-            int valor=archivo.read();
-            while (valor!=-1){
-                System.out.print((char) valor);
-                System.lineSeparator();
-                valor=archivo.read();
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+       File validar=new File(path);
+       if (validar.exists()){
+           try {
+               FileReader archivo=new FileReader(path);
+               int valor=archivo.read();
+               while (valor!=-1){
+                   System.out.print((char) valor);
+                   System.lineSeparator();
+                   valor=archivo.read();
+               }
+           } catch (IOException e) {
+               throw new RuntimeException(e);
+           }
+       } else System.out.println("Este archivo no existe");
+
     }
 
     public static void crearArchivo(String texto,String path) throws IOException {
-        FileWriter newFile= new FileWriter(path);
-        newFile.write(texto);
-        newFile.close();
+        File validar=new File(path);
+       if (!validar.isFile()) {
+           FileWriter newFile= new FileWriter(path);
+           newFile.write(texto);
+           newFile.close();
+       } else {
+           JOptionPane.showMessageDialog(null,"Este archivo ya existe!!");
+       }
         }
     }
---
+++ src/test/java/org/example/FileUtilsTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileUtilsTest {
+
+    @org.junit.jupiter.api.Test
+    void leerArchivo() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void crearArchivo() {
+    }
+}
